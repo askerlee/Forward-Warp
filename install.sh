a257c240fa1aa485fb6196ced1f5835c6770aacc
shaohua <shaohua@bright.ngrn.a-star.edu.sg>
2022-04-30 23:33:35 +0800

    setup install to user dir

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 work_path=$(dirname $(readlink -f $0))
 cd ${work_path}/Forward_Warp/cuda/
-python3 setup.py install | grep "error"
+python3 setup.py install --user | grep "error"
 cd ../../
-python3 setup.py install
+python3 setup.py install --user
